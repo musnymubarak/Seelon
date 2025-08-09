dc1bb64b9f070ea02244128061ef13ad8a6d18c6
musnymubarak <musnymohammed@gmail.com>
2025-08-09 10:21:15 +0530

    Initialize the application

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>
+        Hi There
+      </h2>
+      <Button>
+        Login
+      </Button>
+    </div>
+  );
+}
